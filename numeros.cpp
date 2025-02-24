9751e949151e4547595c26762022a6dbdb1eab9c
Pedro <65288010+Hoshino2123@users.noreply.github.com>
2025-02-24 16:03:51 +0000

    Add files via upload

---
+++ numeros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int n1;
+    cout << "insira 1 numero";
+    cin >> n1;
+
+
+    if (n1%2 ==  0) {
+        cout << "numero par";
+    }
+    else {
+        cout << "numero impar";
+    }
+    return 0;
+}
