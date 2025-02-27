6c590ff78e9d0338f6c5e93cb40f64a31c0cf5a2
Pedro <65288010+Hoshino2123@users.noreply.github.com>
2025-02-27 16:11:50 +0000

    numeros  pares de 2 ate 200

---
+++ vscode/3-numerospares.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int Numero;
+
+cout << "insire um Numero: ";
+
+cin >> Numero;
+
+for(int i = 2; i <= Numero; i += 2) {
+
+cout << i << " ";
+
+}
+
+cout << endl;
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
