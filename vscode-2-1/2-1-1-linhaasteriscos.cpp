120b20bb60399422da6ffe52a635990b2445b17d
Pedro <65288010+Hoshino2123@users.noreply.github.com>
2025-03-10 16:41:15 +0000

    Update 2-1-1-linhaasteriscos.cpp

--- vscode-2-1/2-1-1-linhaasteriscos.cpp
+++ vscode-2-1/2-1-1-linhaasteriscos.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 
 int main () {
-   int tamanho; //variável
+   int valor; //variável
 
-   cout << "podes escrever o tamanho que queres para a linha de *"; // pede ao utilizador pra escrever o tamanho para linha de *
-   cin >> tamanho; // lê o tamanho que escrever, ex:9
+   cout << "podes escrever o valor que queres para a linha de *"; // pede ao utilizador pra escrever o nº para linha de *
+   cin >> valor; // lê o nº que escrever, ex:9
 
-   for(int i=1; i<tamanho; i++) { //começa o for com i que é igual  a 1.
-    //sempre que o i for menor que o tamanho que o user definiu vai adicionar 1.
+   for(int i=1; i<valor; i++) { //começa o for com i que é igual  a 1.
+    //sempre que o i for menor que o valor que o user definiu vai adicionar 1.
     cout << "*";  //escreve a linha de *
    }
 
