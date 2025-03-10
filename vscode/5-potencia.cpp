11290a03c1a28a53813bbcb1396ce20c6849173a
Pedro <65288010+Hoshino2123@users.noreply.github.com>
2025-03-10 15:47:12 +0000

    Update 5-potencia.cpp
    Update 5-potencia.cpp

--- vscode/5-potencia.cpp
+++ vscode/5-potencia.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-    int expoente = 0; //variável
+    int expoente = 0; //variável, o << é o operador de deslocamento á esquerda.
+        //desloca os bits do número à esquerda pelo número de posições especificado à direita do operador. 
+        //Cada deslocamento à esquerda equivale a multiplicar o número por 2.
 
     while (true) { //repete 0 ou + vezes.
         int resultado = 1 << expoente; //faz a potência do 2
@@ -17,3 +19,21 @@ int main() {
 
     return 0; //retorna 0 
 }
+
+#include <iostream> //não ter de usar o std.
+#include <cmath> //bibloteca para funções matemáticas.
+#include <bitset> //escrever números em biinário.
+using namespace std;
+   int main() {
+    cout << " potências de 2: "; //apresenta as potências de 2.
+    //for(int i=9; i>=0; i++) //começa com i que é igual a 9.
+     //depois quando o i é maior ou igual a 0 vai aumentando 1.
+    for(int i = 9; i>=0; i--) { //começa com i que é igual a 9.
+        //depois quando o i é maior ou igual a 0 vai diminuindo 1.
+        cout << pow(2,i) << ' ';
+    }
+
+    
+  
+   }
+
