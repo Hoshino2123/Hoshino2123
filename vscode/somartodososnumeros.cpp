d53cac591221d41b0e77edc1ca2b1aad002651e7
Pedro <65288010+Hoshino2123@users.noreply.github.com>
2025-05-29 16:04:26 +0100

    matéria sobre estruturas e somar tdos os nºs

---
+++ vscode/somartodososnumeros.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int num[TOT][TOT2];
+//troço de código para somar todos os números.
+for(int i=0; i<TOT; i++)
+   for(int j=0;) j<TOt2; J++;
+   SOMA +=NUM[i][j];
+cout << soma;
