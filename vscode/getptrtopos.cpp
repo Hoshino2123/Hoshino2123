b82f0a3f1f748a55ebc957535c06b423935797f0
Pedro <65288010+Hoshino2123@users.noreply.github.com>
2025-07-10 23:11:55 +0100

    Add files via upload

---
+++ vscode/getptrtopos.cpp
@@ -0,0 +1,19 @@
+/* função getptrtopos()
+receber um índice (começa no 1)
+devolve um pointer para o elemento, caso exista, ou nullptr caso não exista
+
+el *getptrtopos(size_t   pos){
+el *ptr =phead;
+while(ptr){
+if(con==pos) break;
+cont++;
+
+ptr==ptr->next;
+}
+return ptr;
+}*/
+
+// phead =getptrtopos()
+
+//while - percorrer lista.
+//for - percorrer array.
